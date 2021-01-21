c1ad9c5670a56fe3c0443a4ad590589f35ff3664
Rupert Smith <rupert@thesett.com>
2021-01-21 13:56:31 +0000

    API router.

--- api/lambda/src/Packages/API.elm
+++ api/lambda/src/Packages/API.elm
@@ -1,7 +1,13 @@
 port module Packages.API exposing (main)
 
+import Json.Decode as Decode exposing (Decoder)
+import Json.Encode as Encode
 import Serverless
-import Serverless.Conn exposing (respond, textBody)
+import Serverless.Conn exposing (jsonBody, method, request, respond, route, textBody)
+import Serverless.Conn.Request exposing (Method(..), Request, asJson, body)
+import Url
+import Url.Parser exposing ((</>), (<?>), int, map, oneOf, s, top)
+import Url.Parser.Query as Query
 
 
 main : Serverless.Program () () () ()
@@ -22,3 +28,23 @@ port requestPort : Serverless.RequestPort msg
 
 
 port responsePort : Serverless.ResponsePort msg
+
+
+
+-- Route and query parsing.
+
+
+type Route
+    = AllPackages
+    | AllPackagesSince Int
+    | Tags
+
+
+routeParser : Url.Url -> Maybe Route
+routeParser =
+    oneOf
+        [ map AllPackages (s "all-packages")
+        , map AllPackagesSince (s "all-packages" </> s "since" </> Url.Parser.int)
+        , map Tags (s "tags")
+        ]
+        |> Url.Parser.parse
