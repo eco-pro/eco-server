566d3cec4528445e112663afa2b8ae323e15d1cf
rupert <github@thesett.com>
2021-01-22 21:52:47 +0000

    Removing files that should not be there.

--- src/index.js
+++
@@ -1,7 +0,0 @@
-var fs = require('fs');
-var glob = require('glob');
-const path = require('path');
-const { spawn } = require('child_process');
-
-const { Elm } = require('./elm.js');
-const app = Elm.Top.init();
