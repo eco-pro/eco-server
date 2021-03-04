a929ef2f03bda583e6e30797c14798c326221b2b
Rupert Smith <rupert@thesett.com>
2021-03-04 18:02:36 +0000

    Defining the build service as running off a queue.

--- infrastructure/lib/eco-server-stack.js
+++ infrastructure/lib/eco-server-stack.js
@@ -3,11 +3,22 @@ const ec2 = require("@aws-cdk/aws-ec2");
 const ecs = require("@aws-cdk/aws-ecs");
 const ecs_patterns = require("@aws-cdk/aws-ecs-patterns");
 const s3 = require('@aws-cdk/aws-s3');
+const sqs = require('@aws-cdk/aws-sqs');
 
 class InfrastructureStack extends cdk.Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
 
+    // VPC Network Segment.
+    const vpc = new ec2.Vpc(this, "eco-server-vpc", {
+      maxAzs: 1
+    });
+
+    // Package Server API.
+
+    // Package Database.
+
+    // Build Artifacts.
     new s3.Bucket(this, 'elm-packages', {
       versioned: true
     });
@@ -16,28 +27,19 @@ class InfrastructureStack extends cdk.Stack {
       versioned: true
     });
 
-    const vpc = new ec2.Vpc(this, "eco-server-vpc", {
-      maxAzs: 1
-    });
+    // Build job processing.
+    const queue = new sqs.Queue(this, "eco-buildjob-queue");
 
     const cluster = new ecs.Cluster(this, "eco-ecs-cluster", {
       vpc: vpc
     });
 
-    new ecs.FargateService(this,
-      "eco-fargate-service", {
-        cluster: cluster,
-        desiredCount: 1,
-        taskDefinition: {
-          compatability: ecs.FARGATE,
-          cpu: 256,
-          taskDefinition: {
-            image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample")
-          },
-          memoryLimitMiB: 2048,
-          publicLoadBalancer: false
-        }
-      });
+    new ecs_patterns.QueueProcessingFargateService(this, "eco-build-service", {
+      cluster,
+      queue,
+      desiredTaskCount: 1,
+      image: ecs.ContainerImage.fromRegistry("amazon/amazon-ecs-sample")
+    });
   }
 }
 
