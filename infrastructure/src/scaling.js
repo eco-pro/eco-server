e64cfdf655a8a1a05f78f7a0e4acb76cd2d5b572
Rupert Smith <rupert@thesett.com>
2021-03-09 17:01:13 +0000

    Outlining what the scaling operation needs to do.

--- infrastructure/src/scaling.js
+++ infrastructure/src/scaling.js
@@ -1,6 +1,13 @@
 export async function handleAlarm(event, context) {
   console.log("Build Job Scaling Service.");
   console.log(event.Records[0].Sns.Message);
+
+  // Determine which action is to be taken based on the alarm name.
+
+  // Set the desired count of the build job service.
+  // ARN for build job service.
+  // Invoke ecs api to update the service with desired count.
+
   return {
     statusCode: 200,
     body: JSON.stringify({ status: "successful" }),
