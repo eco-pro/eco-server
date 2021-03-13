142a939f6b94e5fc93f9f854fb60b67713067b24
Rupert Smith <rupert@thesett.com>
2021-03-13 09:40:00 +0000

    Cloudmap plugin

---
+++ 02-build-api/serverless-cloudmap/index.js
@@ -0,0 +1,63 @@
+'use strict';
+
+class Cloudmap {
+  constructor(serverless, options) {
+    this.serverless = serverless;
+    this.options = options;
+
+    this.commands = {
+      deploy: {
+        lifecycleEvents: ['resources', 'functions'],
+      },
+    };
+
+    this.hooks = {
+      'package:initialize': this.generateCloudformation.bind(this),
+    };
+  }
+
+  generateCloudformation() {
+    const {
+      cloudmap
+    } = this.serverless.service.custom;
+    const rsrc = this.serverless.service.provider.compiledCloudFormationTemplate.Resources;
+    const {
+      services
+    } = cloudmap;
+    services.forEach((service) => {
+      rsrc[service.cfname] = {
+        'Type': 'AWS::ServiceDiscovery::Service',
+        'Properties': {
+          'Description': service.description,
+          'Name': service.name,
+          'DnsConfig': {
+            'DnsRecords': [ { 'TTL' : 100, 'Type' : 'CNAME' } ],
+            'NamespaceId': service.namespace
+          }
+        }
+      };
+      service.instances.forEach((instance) => {
+        rsrc[instance.cfname] = {
+          'Type': "AWS::ServiceDiscovery::Instance",
+          'Properties': {
+            'InstanceAttributes': {
+              'arn': instance.arn,
+              'handler': instance.name,
+              'url': instance.url,
+              'type': 'function',
+              ...instance.config,
+            },
+            'InstanceId': instance.name,
+            'ServiceId': {
+              'Ref': service.cfname,
+            }
+          },
+        };
+      });
+    });
+
+    this.serverless.service.provider.compiledCloudFormationTemplate.Resources = rsrc;
+  };
+}
+
+module.exports = Cloudmap;
