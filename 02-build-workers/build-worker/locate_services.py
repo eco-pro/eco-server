b71fdb43277e3aee31e25396716da103f77534e8
Rupert Smith <rupert@thesett.com>
2021-03-18 16:02:45 +0000

    Script to do service lookup.

---
+++ 02-build-workers/build-worker/locate_services.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+import requests as req
+import re
+import zipfile
+import json
+import hashlib
+import os
+import subprocess
+import pathlib
+import shutil
+import sys
+import time
+import logging
+import boto3
+from botocore.exceptions import ClientError
+
+offline_mode = os.environ.get('OFFLINE_MODE')
+
+if offline_mode == 'true':
+    # Use OFFLINE_MODE=true
+    config = {
+        'PACKAGE_API_ROOT': os.environ.get('PACKAGE_API_ROOT')
+    }
+
+    print(config)
+else:
+    # Use OFFLINE_MODE=false
+    config = {
+        'AWS_CONTAINER_CREDENTIALS_RELATIVE_URI': os.environ.get('AWS_CONTAINER_CREDENTIALS_RELATIVE_URI'),
+        'DISCOVERY_NAMESPACE': 'mydomain.com',
+        'BUILD_API_SERVICE': 'build-api-service'
+    }
+
+    print(config)
+
+    session = boto3.session.Session()
+    print(session.get_credentials().get_frozen_credentials())
+
+
+    # Find the build API service.
+    print("\n=== Looking for the build API service.")
+    discovery_namespace = config['DISCOVERY_NAMESPACE']
+    build_api_service = config['BUILD_API_SERVICE']
+
+    sdclient = session.client('servicediscovery')
+    discovery_response = sdclient.discover_instances(
+        NamespaceName=discovery_namespace,
+        ServiceName=build_api_service
+    )
+
+    if not discovery_response['Instances']:
+        print("Failed to discover the build API service.")
+        quit()
+
+    config['PACKAGE_API_ROOT'] = discovery_response['Instances'][0]['Attributes']['url']
+    print("Got build API root URL: " + config['PACKAGE_API_ROOT'])
+
+
+print('PACKAGE_API_ROOT: ' + config['PACKAGE_API_ROOT'])
