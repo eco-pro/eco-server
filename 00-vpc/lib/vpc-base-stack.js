4b8e13a2ce884e50add9a9c19a415095b5d23314
Rupert Smith <rupert@thesett.com>
2021-04-28 20:14:55 +0100

    Need DNS support to find AWS services.

--- 00-vpc/lib/vpc-base-stack.js
+++ 00-vpc/lib/vpc-base-stack.js
@@ -8,7 +8,6 @@ Set up the VPC for the package server. This includes:
 
 - The VPC itself, tagged with:
     vpc-name = eco-server-vpc
-- A VPC Interface Endpoint to AWS Lambda.
 - A Private Namespace for the VPC as a service registry.
 
 */
@@ -21,18 +20,12 @@ export default class VpcBaseStack extends sst.Stack {
     // VPC Network Segment.
     const vpc = new Vpc(this, "vpc", {
       maxAzs: 1,
-      enableDnsSupport: false,
-      enableDnsHostnames: false,
+      enableDnsSupport: true,
+      enableDnsHostnames: true,
       natGateways: 0,
       subnetConfiguration: [
-        //{ cidrMask: 23, name: 'Isolated', subnetType: SubnetType.ISOLATED }
         { cidrMask: 23, name: 'Public', subnetType: SubnetType.PUBLIC }
       ]
-      // gatewayEndpoints: {
-      //   S3: {
-      //     service: GatewayVpcEndpointAwsService.S3,
-      //   },
-      // }
     });
 
     Tags.of(vpc).add('vpc-name', 'eco-server-vpc');
@@ -41,11 +34,6 @@ export default class VpcBaseStack extends sst.Stack {
      value: vpc.vpcId,
      exportName: app.logicalPrefixedName("VpcId")
     });
-
-    // Add an interface endpoint for invoking ECR and Secrets Manager
-    // vpc.addInterfaceEndpoint('LambdaEndpoint', {
-    //   service: InterfaceVpcEndpointAwsService.LAMBDA,
-    // });
 
     // Add a service discovery namespace.
     const namespace = new servicediscovery.PrivateDnsNamespace(this, 'service-namespace', {
